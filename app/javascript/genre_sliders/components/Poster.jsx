1220cf84beb82b7c5380f280f01f04881a132168
Eric Stephenson <eric.stephenson@rammount.com>
2018-06-23 14:16:46 -0700

    Set hover style with function in SliderContent

--- app/javascript/genre_sliders/components/Poster.jsx
+++ app/javascript/genre_sliders/components/Poster.jsx
@@ -12,24 +12,39 @@ const Poster = (props) => {
     }
 
     const handleMouseOver = function(event) {
-      // const mouseOver = {
-      //   transform: 'scale(1.75)',
-      //   transitionDuration: '400ms',
-      //   transitionTimingFunction: 'cubic-bezier(0.5, 0, 0.1, 1)',
-	    //   transitionDelay: '0ms'
-      // };
-      const mouseOver = 'transform: scale(1.75); transition-duration: 400ms; transition-timing-function: cubic-bezier(0.5, 0, 0.1, 1); transition-delay: 0ms;';
+      const mouseOver = 'transform: scale(1.75) translate3d(0px, 0px, 0px); transition-duration: 400ms; transition-timing-function: cubic-bezier(0.5, 0, 0.1, 1); transition-delay: 0ms;';
       const target = event.target.closest('.poster-container');
       target.setAttribute('style', mouseOver);
     }
 
     const handleMouseOut = function(event) {
       const target = event.target.closest('.poster-container');
-      target.setAttribute('style', '');
+      target.setAttribute('style', 'transform: scale(1.0); transition-duration: 400ms; transition-timing-function: cubic-bezier(0.5, 0, 0.1, 1); transition-delay: 0ms;');
     }
 
+    const renderContainerClass = function(props) {
+      if (props.start) {
+        if (props.index <= props.slideLength) {
+          return `poster-container slide-item-${props.index}`;
+        } else {
+          return 'poster-container';
+        }
+      } else {
+        if (props.index <= props.slideLength + 1) {
+          return `poster-container slide-item-${props.index}`;
+        } else {
+          return 'poster-container';
+        }
+      }
+    }
+
+    const posterStyle = props.buildPosterStyle(props.index);
+
     return (
-      <div className='poster-container' onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
+      <div className={renderContainerClass(props)}
+           style={posterStyle}
+           onMouseOver={props.mouseOver}
+           onMouseOut={props.mouseOut}>
         <div className='poster' style={posterImage}></div>
       </div>
     );
--- app/javascript/genre_sliders/components/SliderContent.jsx
+++ app/javascript/genre_sliders/components/SliderContent.jsx
@@ -10,8 +10,10 @@ export class SliderContent extends Component {
     this.state = {
       slides: this.props.slides,
       slideLength: this.props.slideLength,
-      start: this.props.start
+      start: this.props.start,
+      hoverItem: null
     }
+
     this.transformations = {
       2: '-150%',
       3: '-133.33333333333334%',
@@ -20,10 +22,131 @@ export class SliderContent extends Component {
       6: '-116.66666666666667%'
     }
 
+    this.hoverStyle = {
+      transform: 'scale(1.75) translate3d(0px, 0px, 0px)',
+      transitionDuration: '400ms',
+      transitionTimingFunction: 'cubic-bezier(0.5, 0, 0.1, 1)',
+      transitionDelay: '0ms'
+    }
+
     this.handleMouseOver = this.handleMouseOver.bind(this);
+    this.handleMouseOut = this.handleMouseOut.bind(this);
+    this.determineTranslateX = this.determineTranslateX.bind(this);
+    this.buildPosterStyle = this.buildPosterStyle.bind(this);
   }
 
-  handleMouseOver() {}
+  handleMouseOver(event) {
+    const target = event.target.closest('.poster-container');
+    const slideIndex = parseInt(target.classList[1].slice(-1), 10);
+    
+    this.setState({
+      hoverItem: slideIndex
+    });
+  }
+
+  handleMouseOut(event) {
+    const target = event.target.closest('.poster-container');
+    // return scale value to 1.0
+
+    this.setState({
+      hoverItem: null
+    });
+  }
+
+  determineTranslateX() {
+    const width = document.getElementsByClassName('sliderContent')[0].clientWidth;
+    const slidesNum = this.state.slides.length;
+    return Math.floor((width / slidesNum) - 4);
+  }
+
+  buildPosterStyle(index) {
+    const hoverItem = this.state.hoverItem;
+
+    if (hoverItem || hoverItem === 0) {
+      const slideLength = this.state.slideLength;
+      const translateX = this.determineTranslateX();
+      let transform;
+
+      if (index < hoverItem) {
+        if (hoverItem === slideLength - 1) {
+          transform = `translate3d(-${translateX * 2}px, 0px, 0px)`;
+        } else {
+          transform = `translate3d(-${translateX}px, 0px, 0px)`;
+        }
+
+        return {
+          transform: transform,
+          transitionDuration: '400ms',
+          transitionTimingFunction: 'cubic-bezier(0.5, 0, 0.1, 1)',
+          transitionDelay: '0ms'
+        };
+
+      } else if (index === hoverItem) {
+        let translateHalf;
+
+        if (index === 0) {
+          translateHalf = Math.floor((translateX / 2) + 5);
+          transform = `scale(1.75) translate3d(${translateHalf}px, 0px, 0px)`;
+          return {
+            transform: transform,
+            transitionDuration: '400ms',
+            transitionTimingFunction: 'cubic-bezier(0.5, 0, 0.1, 1)',
+            transitionDelay: '0ms'
+          };
+
+        } else if (index === slideLength - 1) {
+          translateHalf = Math.floor((translateX / 2) + 8);
+          transform = `scale(1.75) translate3d(-${translateHalf}px, 0px, 0px)`;
+          return {
+            transform: transform,
+            transitionDuration: '400ms',
+            transitionTimingFunction: 'cubic-bezier(0.5, 0, 0.1, 1)',
+            transitionDelay: '0ms'
+          };
+
+        } else {
+          return {
+            transform: 'scale(1.75) translate3d(0px, 0px, 0px)',
+            transitionDuration: '400ms',
+            transitionTimingFunction: 'cubic-bezier(0.5, 0, 0.1, 1)',
+            transitionDelay: '0ms'
+          };
+        }
+
+      } else if (index > hoverItem) {
+        if (hoverItem === slideLength - 1) {
+          return {};
+
+        } else if (hoverItem === 0) {
+          transform = `translate3d(${translateX * 2}px, 0px, 0px)`;
+          return {
+            transform: transform,
+            transitionDuration: '400ms',
+            transitionTimingFunction: 'cubic-bezier(0.5, 0, 0.1, 1)',
+            transitionDelay: '0ms'
+          };
+
+        } else {
+          transform = `translate3d(${translateX}px, 0px, 0px)`;
+          return {
+            transform: transform,
+            transitionDuration: '400ms',
+            transitionTimingFunction: 'cubic-bezier(0.5, 0, 0.1, 1)',
+            transitionDelay: '0ms'
+          };
+        }
+
+      }
+
+    } else {
+      return {};
+    }
+  }
+
+  componentDidUpdate() {
+    console.log('SliderContent Updated');
+    console.log(this.state);
+  }
 
   render() {
     const slides = this.state.slides;
@@ -35,10 +158,17 @@ export class SliderContent extends Component {
     if (slides && slides.length > 0) {
       if (this.state.start === true) {
         return (
-          <div className="sliderContent" style={{ transform: '' }}>
+          <div className="sliderContent">
             {
               slides.map((slide, index) =>
-                <Poster key={index} movie={slide} />
+                <Poster key={index}
+                        index={index}
+                        movie={slide}
+                        slideLength={slideLength}
+                        start={this.state.start}
+                        mouseOver={this.handleMouseOver}
+                        mouseOut={this.handleMouseOut}
+                        buildPosterStyle={this.buildPosterStyle} />
               )
             }
           </div>
@@ -48,7 +178,13 @@ export class SliderContent extends Component {
           <div className="sliderContent" style={slideOver}>
             {
               slides.map((slide, index) =>
-                <Poster key={index} movie={slide} />
+                <Poster key={index}
+                        index={index}
+                        movie={slide}
+                        slideLength={slideLength}
+                        start={this.state.start}
+                        mouseOver={this.handleMouseOver}
+                        mouseOut={this.handleMouseOut} />
               )
             }
           </div>
