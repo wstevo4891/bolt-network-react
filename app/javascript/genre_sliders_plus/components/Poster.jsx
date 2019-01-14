53b57167af331c69ce2813c39de38613e42b1987
Eric Stephenson <eric.stephenson@rammount.com>
2019-01-13 17:57:36 -0800

    Use PosterService in Poster.jsx component, Refactor PosterService.js

--- app/javascript/genre_sliders_plus/components/Poster.jsx
+++ app/javascript/genre_sliders_plus/components/Poster.jsx
@@ -2,42 +2,46 @@
 
 import React from 'react';
 
-import * as actions from '../actions/buildPosterStyle';
+// import * as actions from '../actions/buildPosterStyle';
+import PosterService from '../services/PosterService';
 
 const Poster = (props) => {
   const movie = props.movie;
+  console.log(`hoverItem: ${props.hoverItem}`);
 
-  if (typeof movie === 'object') {
-    const posterImage = {
-      backgroundImage: `url(${movie.photo.url})`,
-      backgroundSize: '100% 100%'
-    }
+  if (typeof movie !== 'object') return null;
 
-    const containerClass = actions.buildContainerClass(props);
-
-    let posterStyle;
-
-    if (props.start) {
-      posterStyle = actions.buildPosterStyle(props);
-    } else {
-      posterStyle = actions.buildNextPosterStyle(props);
-    }
-
-    return (
-      <a href={`/movies/${movie.id}`}>
-        <div
-          className={containerClass}
-          style={posterStyle}
-          onMouseOver={props.mouseOver}
-          onMouseOut={props.mouseOut}
-        >
-          <div className='poster' style={posterImage}></div>
-        </div>
-      </a>
-    );
-  } else {
-    return null;
+  const posterImage = {
+    backgroundImage: `url(${movie.photo.url})`,
+    backgroundSize: '100% 100%'
   }
+
+  const service = new PosterService(props);
+
+  // const containerClass = actions.buildContainerClass(props);
+  const containerClass = service.containerClass();
+
+  // let posterStyle;
+
+  // if (props.start) {
+  //   posterStyle = actions.buildPosterStyle(props);
+  // } else {
+  //   posterStyle = actions.buildNextPosterStyle(props);
+  // }
+  const posterStyle = service.posterStyle();
+
+  return (
+    <a href={`/movies/${movie.id}`}>
+      <div
+        className={containerClass}
+        style={posterStyle}
+        onMouseOver={props.mouseOver}
+        onMouseOut={props.mouseOut}
+      >
+        <div className='poster' style={posterImage}></div>
+      </div>
+    </a>
+  );
 }
 
 export default Poster;
--- app/javascript/genre_sliders_plus/services/PosterService.js
+++ app/javascript/genre_sliders_plus/services/PosterService.js
@@ -5,6 +5,7 @@ export default class PosterService {
     this.start = props.start;
     this.index = props.index;
     this.slideLength = props.slideLength;
+    this.limit = (props.slideLength * 2) + 1;
     this.hoverItem = props.hoverItem;
     this.hoverStyle = {
       transform: 'translate3d(0px, 0px, 0px)',
@@ -23,11 +24,8 @@ export default class PosterService {
       }
 
     } else {
-      const begin = this.slideLength;
-      const limit = (this.slideLength * 2) + 1;
-
-      if (this.index >= begin && this.index <= limit) {
-        const diff = (this.index - this.slideLength);
+      if (this.index >= this.slideLength && this.index <= this.limit) {
+        const diff = this.index - this.slideLength;
         return `poster-container slide-item-${diff}`;
 
       } else {
@@ -37,122 +35,67 @@ export default class PosterService {
   }
 
   posterStyle = () => {
-    if (this.start) {
-      return startPosterStyle();
-    } else {
-      return buildNextPosterStyle();
-    }
-  }
+    if (this.hoverItem === null) return this.hoverStyle;
 
-  startPosterStyle = () => {
-    if (!this.hoverItem || this.hoverItem !== 0) return this.hoverStyle;
-
-    const translateX = this.calcTranslateX();
-
-    if (this.index < this.hoverItem) {
-      this.hoverStyle.transform = this.determineTransform(translateX, this.hoverItem, this.slideLength);
-
-    } else if (this.index === this.hoverItem) {
-      if (this.index === 0) {
-        const translateHalf = Math.floor((translateX / 2) + 5);
-        this.hoverStyle.transform = this.translate3D(translateHalf, true);
-
-      } else if (this.index === this.slideLength - 1) {
-        const translateHalf = Math.floor((translateX / 2) + 8);
-        this.hoverStyle.transform = this.translate3D(-translateHalf, true);
-
-      } else {
-        this.hoverStyle.transform = this.translate3D(0, true);
-      }
-
-    } else if (this.index > this.hoverItem) {
-      if (this.hoverItem === this.slideLength - 1) {
-        return {};
-
-      } else if (this.hoverItem === 0) {
-        this.hoverStyle.transform = this.translate3D(translateX * 2);
-
-      } else {
-        this.hoverStyle.transform = this.translate3D(translateX);
-      }
-    }
-
-    return this.hoverStyle;
-  }
-
-  determineStartTransform = (tx) => {
-    if (this.hoverItem === this.slideLength - 1) {
-      // `translate3d(-${translateX * 2}px, 0px, 0px)`;
-      return this.translate3D(-tx * 2);
-    } else {
-      // `translate3d(-${translateX}px, 0px, 0px)`;
-      return this.translate3D(-tx);
-    }
-  }
-
-  determineTransform = (tx, hover, end) => {
-    // if (this.hoverItem === this.slideLength - 1) {
-    if (hover === end - 1) {
-      // `translate3d(-${translateX * 2}px, 0px, 0px)`;
-      return this.translate3D(-tx * 2);
-    } else {
-      // `translate3d(-${translateX}px, 0px, 0px)`;
-      return this.translate3D(-tx);
-    }
-  }
-
-  nextPosterStyle = () => {
-    if (!this.hoverItem) return this.hoverStyle;
-
-    const begin = this.slideLength;
-    const end = (this.slideLength * 2) + 1;
-
-    if (!this.index >= begin && !this.index <= end) return this.hoverStyle;
-
-    const translateX = calcTranslateX();
-    const actualHover = this.hoverItem + begin;
-
-    if (this.index < actualHover) {
-      if (actualHover === begin + 1) return this.hoverStyle;
-
-      if (actualHover === end - 1) {
-        this.hoverStyle.transform = translate3D(-translateX * 2);
-      } else {
-        this.hoverStyle.transform = translate3D(-translateX);
-      }
-
-    } else if (this.index === actualHover) {
-      if (this.index === begin + 1) {
-        const translateHalf = Math.floor((translateX / 2) + 5);
-        this.hoverStyle.transform = translate3D(translateHalf, true);
-
-      } else if (this.index === end - 1) {
-        const translateHalf = Math.floor((translateX / 2) + 8);
-        hoverStyle.transform = translate3D(-translateHalf, true);
-
-      } else {
-        // hoverStyle.transform = 'scale(1.75) translate3d(0px, 0px, 0px)';
-        this.hoverStyle.transform = translate3D(0, true);
-      }
-
-    } else if (this.index > actualHover) {
-      if (actualHover === end - 1) return this.hoverStyle;
-
-      if (actualHover === begin + 1) {
-        this.hoverStyle.transform = translate3D(translateX * 2);
-
-      } else {
-        // transform = `translate3d(${translateX}px, 0px, 0px)`;
-        this.hoverStyle.transform = translate3D(translateX);
-      }
-    }
-
-    return this.hoverStyle;
+    this.translateX = this.calcTranslateX();
+    this.hover = this.start ? this.hoverItem : this.hoverItem + this.slideLength;
+    this.end = this.start ? this.slideLength : this.limit;
+  
+    return this.buildPosterStyle();
   }
 
   calcTranslateX = () => {
     const width = document.getElementsByClassName('poster-container')[0].clientWidth;
     return Math.round(width * 0.38);
+  }
+
+  buildPosterStyle = () => {
+    if (this.index < this.hover) {
+      if (this.start === false && this.hover === this.slideLength + 1) return this.hoverStyle;
+  
+      this.hoverStyle.transform = this.negativeTranslate();
+  
+    } else if (this.index === this.hover) {
+      this.currentPositionStyle();
+  
+    } else if (this.index > this.hover) {
+      if (this.hover === this.end - 1) return this.hoverStyle;
+  
+      this.afterHoverStyle();
+    }
+  
+    return this.hoverStyle;
+  }
+  
+  currentPositionStyle = () => {
+    if ((this.start && this.index === 0) || this.index === this.slideLength + 1) {
+      const translateHalf = Math.floor((this.translateX / 2) + 5);
+      this.hoverStyle.transform = this.translate3D(translateHalf, true);
+  
+    } else if (this.index === this.end - 1) {
+      const translateHalf = Math.floor((this.translateX / 2) + 8);
+      this.hoverStyle.transform = this.translate3D(-translateHalf, true);
+  
+    } else {
+      this.hoverStyle.transform = this.translate3D(0, true);
+    }
+  }
+  
+  afterHoverStyle = () => {
+    if ((this.start && this.hover === 0) || this.hover === this.slideLength + 1) {
+      this.hoverStyle.transform = this.translate3D(this.translateX * 2);
+  
+    } else {
+      this.hoverStyle.transform = this.translate3D(this.translateX);
+    }
+  }
+
+  negativeTranslate = () => {
+    if (this.hover === this.end - 1) {
+      return this.translate3D(-this.translateX * 2);
+    } else {
+      return this.translate3D(-this.translateX);
+    }
   }
 
   translate3D = (x, scale = false) => {
@@ -163,182 +106,3 @@ export default class PosterService {
     }
   }
 }
-
-
-// ============================================================================
-
-
-// export function buildPosterStyle(props) {
-//   const hoverItem = props.hoverItem;
-//   const index = props.index;
-//   let hoverStyle = {
-//     transform: 'translate3d(0px, 0px, 0px)',
-//     transitionDuration: '400ms',
-//     transitionTimingFunction: 'cubic-bezier(0.5, 0, 0.1, 1)',
-//     transitionDelay: '0ms'
-//   };
-
-//   if (hoverItem || hoverItem === 0) {
-//     const slideLength = props.slideLength;
-//     const translateX = determineTranslateX();
-//     let transform;
-
-//     if (index < hoverItem) {
-//       if (hoverItem === slideLength - 1) {
-//         transform = `translate3d(-${translateX * 2}px, 0px, 0px)`;
-//       } else {
-//         transform = `translate3d(-${translateX}px, 0px, 0px)`;
-//       }
-
-//       hoverStyle.transform = transform;
-//       return hoverStyle;
-
-//     } else if (index === hoverItem) {
-//       let translateHalf;
-
-//       if (index === 0) {
-//         translateHalf = Math.floor((translateX / 2) + 5);
-//         transform = `scale(1.75) translate3d(${translateHalf}px, 0px, 0px)`;
-//         hoverStyle.transform = transform;
-//         return hoverStyle;
-
-//       } else if (index === slideLength - 1) {
-//         translateHalf = Math.floor((translateX / 2) + 8);
-//         transform = `scale(1.75) translate3d(-${translateHalf}px, 0px, 0px)`;
-//         hoverStyle.transform = transform;
-//         return hoverStyle;
-
-//       } else {
-//         hoverStyle.transform = 'scale(1.75) translate3d(0px, 0px, 0px)';
-//         return hoverStyle;
-//       }
-
-//     } else if (index > hoverItem) {
-//       if (hoverItem === slideLength - 1) {
-//         return {};
-
-//       } else if (hoverItem === 0) {
-//         transform = `translate3d(${translateX * 2}px, 0px, 0px)`;
-//         hoverStyle.transform = transform;
-//         return hoverStyle;
-
-//       } else {
-//         transform = `translate3d(${translateX}px, 0px, 0px)`;
-//         hoverStyle.transform = transform;
-//         return hoverStyle;
-//       }
-//     }
-
-//   } else {
-//     return hoverStyle;
-//   }
-// }
-
-// export function buildNextPosterStyle(props) {
-//   const hoverItem = props.hoverItem;
-//   const index = props.index;
-
-//   let hoverStyle = {
-//     transform: 'translate3d(0px, 0px, 0px)',
-//     transitionDuration: '400ms',
-//     transitionTimingFunction: 'cubic-bezier(0.5, 0, 0.1, 1)',
-//     transitionDelay: '0ms'
-//   };
-
-//   if (hoverItem) {
-//     const slideLength = props.slideLength;
-//     const translateX = determineTranslateX();
-//     const begin = slideLength;
-//     const end = (slideLength * 2) + 1;
-//     const actualHover = hoverItem + begin;
-//     let transform;
-
-//     if (index >= begin && index <= end) {
-//       if (index < actualHover) {
-//         if (actualHover === end - 1) {
-//           transform = `translate3d(-${translateX * 2}px, 0px, 0px)`;
-
-//         } else if (actualHover === begin + 1) {
-//           return hoverStyle;
-
-//         } else {
-//           transform = `translate3d(-${translateX}px, 0px, 0px)`;
-//         }
-
-//         hoverStyle.transform = transform;
-//         return hoverStyle;
-
-//       } else if (index === actualHover) {
-//         let translateHalf;
-
-//         if (index === begin + 1) {
-//           translateHalf = Math.floor((translateX / 2) + 5);
-//           transform = `scale(1.75) translate3d(${translateHalf}px, 0px, 0px)`;
-//           hoverStyle.transform = transform;
-//           return hoverStyle;
-
-//         } else if (index === end - 1) {
-//           translateHalf = Math.floor((translateX / 2) + 8);
-//           transform = `scale(1.75) translate3d(-${translateHalf}px, 0px, 0px)`;
-//           hoverStyle.transform = transform;
-//           return hoverStyle;
-
-//         } else {
-//           hoverStyle.transform = 'scale(1.75) translate3d(0px, 0px, 0px)';
-//           return hoverStyle;
-//         }
-
-//       } else if (index > actualHover) {
-//         if (actualHover === begin + 1) {
-//           transform = `translate3d(${translateX * 2}px, 0px, 0px)`;
-//           hoverStyle.transform = transform;
-//           return hoverStyle;
-        
-//         } else if (actualHover === end - 1) {
-//           return hoverStyle;
-
-//         } else {
-//           transform = `translate3d(${translateX}px, 0px, 0px)`;
-//           hoverStyle.transform = transform;
-//           return hoverStyle;
-//         }
-//       }
-
-//     } else {
-//       return hoverStyle;
-//     }
-
-//   } else {
-//     return hoverStyle;
-//   }
-// }
-
-// export function determineTranslateX() {
-//   const width = document.getElementsByClassName('poster-container')[0].clientWidth;
-//   return Math.round(width * 0.38);
-// }
-
-// export function buildContainerClass(props) {
-//   const slideLength = props.slideLength;
-//   const index = props.index;
-
-//   if (props.start) {
-//     if (index <= slideLength) {
-//       return `poster-container slide-item-${props.index}`;
-//     } else {
-//       return 'poster-container slider-item-';
-//     }
-
-//   } else {
-//     const begin = slideLength;
-//     const limit = (slideLength * 2) + 1;
-
-//     if (index >= begin && index <= limit) {
-//       const diff = (index - slideLength);
-//       return `poster-container slide-item-${diff}`;
-
-//     } else {
-//       return 'poster-container slider-item-';
-//     }
-//   }
-// }
