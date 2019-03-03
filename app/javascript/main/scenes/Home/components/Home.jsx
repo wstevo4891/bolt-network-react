fcbfe8a919254989cfe107c5ab06abe5f05b37a5
Eric Stephenson <ericb.stephenson@gmail.com>
2019-03-03 15:12:38 -0800

    Update HTML and styles of home page

--- app/javascript/main/scenes/Home/components/Home.jsx
+++ app/javascript/main/scenes/Home/components/Home.jsx
@@ -12,8 +12,8 @@ const Home = (props) => (
     <div className="container">
       <div className="row justify-content-center">
         <div className="supporting">
-          <h2>A world of movies at your fingertips.</h2>
-          <p>Choose from the latest titles, with new movies added every day.</p>
+          <h1>A world of movies at your fingertips</h1>
+          <p>Choose from the latest titles, with new movies added every day</p>
         </div>
       </div>
     </div>
@@ -22,8 +22,8 @@ const Home = (props) => (
 
     <div className="feature">
       <div className="container">
-        <h1>Available everywhere</h1>
-        <p>Start watching on one device, and pick up where you left off on another device.</p>
+        <h2>Available everywhere</h2>
+        <p>Start watching on one device, and pick up where you left off on another device</p>
       </div>
     </div>
   </main>
