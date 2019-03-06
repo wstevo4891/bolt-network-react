ed11e4c59a74bb559430afcb5fbb1fce2df41d1a
Eric Stephenson <ericb.stephenson@gmail.com>
2019-03-05 22:04:59 -0800

    Edit movie genres in movie seeds yaml

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,8 @@ ActiveRecord::Schema.define(version: 20180118041414) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
+    t.string "plural"
+    t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
