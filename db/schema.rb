2fe07da1e376b542b0f4d87a93bbdfed6d55fc57
Eric Stephenson <ericb.stephenson@gmail.com>
2019-11-25 23:03:23 -0800

    Add volume for db service to save development database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180118041414) do
+ActiveRecord::Schema.define(version: 2018_01_18_041414) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
