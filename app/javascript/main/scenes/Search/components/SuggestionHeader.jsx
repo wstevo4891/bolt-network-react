b299445a3d59b3c893ee595ce8c3d2b7e14076a4
Eric Stephenson <ericb.stephenson@gmail.com>
2019-12-13 08:41:33 -0800

    Refactor SuggestionHeader.jsx

--- app/javascript/main/scenes/Search/components/SuggestionHeader.jsx
+++ app/javascript/main/scenes/Search/components/SuggestionHeader.jsx
@@ -1,26 +1,28 @@
 import React from 'react'
 
-const SuggestionHeader = (props) => {
-  if (props.suggestionId === undefined) return null
+const headerText = (type, suggestion) => {
+  if (type === 'genre') {
+    return `Titles in: ${suggestion}`
 
-  const type = props.suggestionId.split('_')[1]
-  
-  const evalHeader = () => {
-    if (type === 'genre') {
-      return `Titles in: ${props.suggestion}`
+  } else if (type === 'person') {
+    return `Results for ${suggestion} and more fan favorites`
 
-    } else if (type === 'person') {
-      return `Results for ${props.suggestion} and more fan favorites`
-
-    } else if (type === 'movie') {
-      return `Titles related to ${props.suggestion}`
-    }
+  } else if (type === 'movie') {
+    return `Titles related to ${suggestion}`
   }
+}
+
+const SuggestionHeader = ({ suggestionId, suggestion }) => {
+  if (suggestionId === undefined) return null
+
+  const type = suggestionId.split('_')[1]
 
   return(
     <div className="col-12">
       <div className="suggestionHeader">
-        <div className="title">{evalHeader()}</div>
+        <div className="title">
+          {headerText(type, suggestion)}
+        </div>
       </div>
     </div>
   )
