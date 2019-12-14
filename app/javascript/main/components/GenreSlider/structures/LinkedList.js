702172a102faa243e7f2f6d1176d0570b9c59787
Eric Stephenson <ericb.stephenson@gmail.com>
2019-12-14 10:24:41 -0800

    Remove unnecessary methods from LinkedList.js

--- app/javascript/main/components/GenreSlider/structures/LinkedList.js
+++ app/javascript/main/components/GenreSlider/structures/LinkedList.js
@@ -1,6 +1,6 @@
 // Doubly Linked List
 
-import Node from './Node';
+import Node from './Node'
 
 export default class LinkedList {
   constructor() {
@@ -46,59 +46,5 @@ export default class LinkedList {
     }
 
     return currentNode
-  }
-
-  remove = (position) => {
-    let currentNode = this.head
-
-    // 1st use-case: an invalid position
-    this.checkPosition()
-
-    // 2nd use-case: the first node is removed
-    if (position === 1) {
-      this.removeFirstNode(currentNode)
-
-    // 3rd use-case: the last node is removed
-    } else if (position === this._length) {
-      this.tail = this.tail.previous
-      this.tail.next = null
-
-    // 4th use-case: a middle node is removed
-    } else {
-      this.removeMiddleNode(currentNode, position)
-    }
-
-    this._length--
-
-    return 'Node was successfully removed.'
-  }
-
-  removeFirstNode = (currentNode) => {
-    this.head = currentNode.next
-
-    // there is a second node
-    if (!this.head) {
-      this.head.previous = null
-
-    // there is no second node
-    } else {
-      this.tail = null
-    }
-  }
-
-  removeMiddleNode = (currentNode, position) => {
-    let count = 1
-
-    while (count < position) {
-      currentNode = currentNode.next
-      count++
-    }
-
-    const beforeNodeToDelete = currentNode.previous
-    const afterNodeToDelete = currentNode.next
-
-    beforeNodeToDelete.next = afterNodeToDelete
-    afterNodeToDelete.previous = beforeNodeToDelete
-    currentNode = null
   }
 }
