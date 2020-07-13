d8985a86a889f93ddeb55a043f5b196446961f91
Eric Stephenson <ericb.stephenson@gmail.com>
2020-07-12 22:08:36 -0700

    Add Button component

---
+++ app/javascript/main/components/Button/index.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Button = (props) => (
+  <button
+    id={props.id}
+    className={props.buttonClass}
+    aria-controls={props.ariaControls}
+    aria-expanded={props.ariaExpanded}
+    aria-haspopup={props.ariaHasPopup}
+    aria-label={props.ariaLabel}
+    data-toggle={props.dataToggle}
+    data-target={props.dataTarget}
+    onClick={props.handleClick}
+    type="button"
+  />
+)
+
+Button.propTypes = {
+  ariaControls: PropTypes.string,
+  ariaExpanded: PropTypes.string,
+  ariaHasPopup: PropTypes.string,
+  ariaLabel: PropTypes.string,
+  buttonClass: PropTypes.string,
+  dataToggle: PropTypes.string,
+  dataTarget: PropTypes.string,
+  handleClick: PropTypes.func,
+  id: PropTypes.string,
+}
+
+Button.defaultProps = {
+  ariaControls: null,
+  ariaExpanded: null,
+  ariaHasPopup: null,
+  ariaLabel: null,
+  buttonClass: null,
+  dataToggle: null,
+  dataTarget: null,
+  handleClick: () => void {},
+  id: null,
+}
