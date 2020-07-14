94890afa78c4273ad9464bbd3529fb193953ae2b
Eric Stephenson <ericb.stephenson@gmail.com>
2020-07-14 00:12:59 -0700

    Use constant for MovieTitle max length

--- app/javascript/main/components/PosterList/components/PosterControls/components/MovieTitle.jsx
+++ app/javascript/main/components/PosterList/components/PosterControls/components/MovieTitle.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+const MAX_LENGTH = 40
+
 const MovieTitle = ({ title }) => {
-  if (title.length > 40) {
-    return <h3>{title.substring(0, 40) + '...'}</h3>
+  if (title.length > MAX_LENGTH) {
+    return <h3>{title.substring(0, MAX_LENGTH) + '...'}</h3>
   } else {
     return <h3>{title}</h3>
   }
