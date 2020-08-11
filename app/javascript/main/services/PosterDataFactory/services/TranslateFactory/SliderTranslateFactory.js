b5b832d9ec6c75a7f0d39f995f2fb4721c073bed
Eric Stephenson <ericb.stephenson@gmail.com>
2020-08-11 12:09:49 -0700

    Save changes to TranslateFactory

--- app/javascript/main/services/PosterDataFactory/services/TranslateFactory/SliderTranslateFactory.js
+++ app/javascript/main/services/PosterDataFactory/services/TranslateFactory/SliderTranslateFactory.js
@@ -48,27 +48,41 @@ function currentPositionTranslate(index) {
   return translate3D(translate, true)
 }
 
+// function currentTranslateX(index, params) {
+//   const { end, translateX } = params
+
+//   if (index === 0) {
+//     return Math.floor((translateX / 2) + 5)
+
+//   } else if (index === end) {
+//     return -Math.floor((translateX / 2) + 8)
+
+//   } else return 0
+// }
+
 function currentTranslateX(index, params) {
-  const { end, translateX } = params
+  if (bookEndPosition(index)) {
+    return Math.floor((params.translateX / 2) + 5)
 
-  if (index === 0) {
-    return Math.floor((translateX / 2) + 5)
-
-  } else if (index === end) {
-    return -Math.floor((translateX / 2) + 8)
+  } else if (index === params.end) {
+    return -Math.floor((params.translateX / 2) + 8)
 
   } else return 0
 }
-  
+
+function bookEndPosition(params) {
+  return (params.start && params.hoverItem === 0) ||
+    params.hoverItem === params.slideLength + 1
+}
+
 function afterHoverTranslate(params) {
-  const { end, hoverItem, translateX } = params
+  if (params.hoverItem === this.end) return null
 
-  if (hoverItem === 0 || hoverItem === end) {
-    return translate3D(translateX * 2)
-
-  } else {
-    return translate3D(translateX)
+  if (bookEndPosition(params)) {
+    return translate3D(params.translateX * 2)
   }
+
+  return translate3D(params.translateX)
 }
 
 function translate3D(x, scale = false) {
---
+++ app/javascript/main/services/PosterDataFactory/services/TranslateFactory/helpers.js
@@ -0,0 +1,17 @@
+export function negativeTranslate(params) {
+  const { end, hoverItem, translateX } = params
+
+  if (hoverItem === end) {
+    return translate3D(-translateX * 2)
+  } else {
+    return translate3D(-translateX)
+  }
+}
+
+function translate3D(x, scale = false) {
+  const translate = `translate3d(${x}px, 0px, 0px)`
+
+  if (scale) return `scale(1.75) ${translate}`
+
+  return translate
+}
